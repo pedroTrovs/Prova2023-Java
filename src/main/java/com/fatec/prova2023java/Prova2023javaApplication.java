fa083f568325fcf4d86d3615e82ffd104e588edc
pedroTrovs <pedrotrovilho@gmail.com>
2023-06-20 09:32:12 -0300

    config inicial

---
+++ src/main/java/com/fatec/prova2023java/Prova2023javaApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.prova2023java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova2023javaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prova2023javaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fatec/prova2023java/Prova2023javaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.prova2023java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova2023javaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
